78b51b1a4667a9ff9716cc19bb654b6681e1fa56
Yiqian <78885507+YiqianMao0502@users.noreply.github.com>
2023-02-28 16:28:22 +0000

    Delete env_0 directory

--- env_0/cleanup-fluent-node730.pri.csf3.alces.network-13052.sh
+++
@@ -1,9 +0,0 @@
-/opt/apps/apps/binapps/fluent/19.2/v192/fluent/bin/fluent-cleanup.pl node730.pri.csf3.alces.network 41206 CLEANUP_EXITING
-
-LOCALHOST=`hostname -s`
-if [[ node730.pri.csf3.alces.network == "$LOCALHOST"* ]]; then kill -9 13763; else ssh node730.pri.csf3.alces.network kill -9 13763; fi
-if [[ node730.pri.csf3.alces.network == "$LOCALHOST"* ]]; then kill -9 13762; else ssh node730.pri.csf3.alces.network kill -9 13762; fi
-if [[ node730.pri.csf3.alces.network == "$LOCALHOST"* ]]; then kill -9 13052; else ssh node730.pri.csf3.alces.network kill -9 13052; fi
-if [[ node730.pri.csf3.alces.network == "$LOCALHOST"* ]]; then kill -9 12725; else ssh node730.pri.csf3.alces.network kill -9 12725; fi
-
-rm -f /net/scratch2/m83358ym/Workingfolder/env_0/cleanup-fluent-node730.pri.csf3.alces.network-13052.sh
